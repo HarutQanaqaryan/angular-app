aa7ca064e3b4086652bdab33b901beeeccea5894
serj1981 <polovinkin.sergey@gmail.com>
2024-02-26 13:44:29 +0300

    added

--- src/app/shared/mock-datas/columns.ts
+++ src/app/shared/mock-datas/columns.ts
@@ -1,7 +1,7 @@
 export const tableColumns = [
   { title: 'ID задачи', type: 'title' },
-  { title: 'Дата создания', type: 'created' },
-  { title: 'Критичность', type: 'type' },
-  { title: 'Статус', type: 'status' },
+  { title: 'Дате создания', type: 'created' },
+  { title: 'Критичности', type: 'type' },
+  { title: 'Статусу', type: 'status' },
   { title: 'Действия' },
 ];
